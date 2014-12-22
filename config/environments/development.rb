8ca6eccba1ca1832da1cb32984c3acdc0255d652
astoimenov <alexander.stoimenov@outlook.com>
2014-12-23 01:57:22 +0200

    Simple refactoring

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,7 +10,7 @@ Blog::Application.configure do
   config.eager_load = false
 
   # Show full error reports and disable caching.
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local = true
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
@@ -30,5 +30,5 @@ Blog::Application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = {host: 'localhost', port: 3000}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,9 +14,9 @@
 ActiveRecord::Schema.define(version: 20141222230844) do
 
   create_table "comments", force: true do |t|
-    t.string   "name"
-    t.text     "body"
-    t.integer  "post_id"
+    t.string "name"
+    t.text "body"
+    t.integer "post_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -24,23 +24,23 @@ ActiveRecord::Schema.define(version: 20141222230844) do
   add_index "comments", ["post_id"], name: "index_comments_on_post_id"
 
   create_table "posts", force: true do |t|
-    t.string   "title"
-    t.text     "body"
+    t.string "title"
+    t.text "body"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
